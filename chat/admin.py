858cb8685aa14699aaa3c04fe3f3145dc9e50ce5
SergeyLebidko <it.sergeyler@mail.ru>
2020-07-13 21:15:29 +0300

    Удалена модель Chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
-from .models import Chat, Message
-
-
-@admin.register(Chat)
-class ChatAdmin(admin.ModelAdmin):
-    list_display = ['users_list']
-
-    def users_list(self, rec):
-        return '/'.join([user.username for user in rec.users.all()])
-
-    users_list.short_description = 'Пользователи в чате'
+from .models import Message
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ['chat', 'dt', 'text']
+    list_display = ['dt', 'text', 'sender', 'receiver']
+    list_display_links = ['text']
--- chat/models.py
+++ chat/models.py
@@ -2,20 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Chat(models.Model):
-
-    users = models.ManyToManyField(User, verbose_name='Пользователи')
-
-    class Meta:
-        verbose_name = 'Чат'
-        verbose_name_plural = 'Чаты'
-
-
 class Message(models.Model):
 
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, verbose_name='Сообщение')
     dt = models.DateTimeField(auto_now=True, verbose_name='Дата/время сообщения')
     text = models.TextField(blank=True, verbose_name='Текст сообщения')
+
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='send_messages',
+        verbose_name='Отправитель'
+    )
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='receive_messages',
+        verbose_name='Получатель'
+    )
 
     class Meta:
         verbose_name = 'Сообщение'
