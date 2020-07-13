b8a50aa6d039497a959ba2cfc1a2af7c72c397c1
SergeyLebidko <it.sergeyler@mail.ru>
2020-07-13 20:28:36 +0300

    Добавлены модели для хранения чатов и сообщений

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Chat, Message
 
-# Register your models here.
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ['users_list']
+
+    def users_list(self, rec):
+        return '/'.join([user.username for user in rec.users.all()])
+
+    users_list.short_description = 'Пользователи в чате'
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['chat', 'dt', 'text']
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Chat(models.Model):
+
+    users = models.ManyToManyField(User, verbose_name='Пользователи')
+
+    class Meta:
+        verbose_name = 'Чат'
+        verbose_name_plural = 'Чаты'
+
+
+class Message(models.Model):
+
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, verbose_name='Сообщение')
+    dt = models.DateTimeField(auto_now=True, verbose_name='Дата/время сообщения')
+    text = models.TextField(blank=True, verbose_name='Текст сообщения')
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
