16f36fde3b85b42acb7648cc1c01327074b23fe2
SergeyLebidko <it.sergeyler@mail.ru>
2020-07-13 14:57:00 +0300

    Добавлены статические файлы и первый контроллер

--- PyChat/settings.py
+++ PyChat/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'chat'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
