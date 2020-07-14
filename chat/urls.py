b37cec11c37add7ce0e4c1cc1e4a7a7dfe387601
SergeyLebidko <it.sergeyler@mail.ru>
2020-07-14 13:48:04 +0300

    Добавлен rest-метод для получения списка пользователей

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,13 @@
-from .views import index
+from .views import index, UserViewSet
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
 
 urlpatterns = [
     path('index/', index, name='index')
 ]
+
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='user')
+
+urlpatterns.extend(router.urls)
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
 
 def index(request):
@@ -11,3 +14,8 @@ def index(request):
         'msg': msg
     }
     return render(request, 'chat/index.html', context=context)
+
+
+class UserViewSet(ReadOnlyModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
