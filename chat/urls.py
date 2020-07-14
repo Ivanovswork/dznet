40faa5a492bb160b0bcfc2e06acc0558aaac37ed
SergeyLebidko <it.sergeyler@mail.ru>
2020-07-14 15:48:06 +0300

    Добавлена форма входа на сайт

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
-from .views import index, UserViewSet
+from .views import index, UserViewSet, Login
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 
 urlpatterns = [
-    path('index/', index, name='index')
+    path('index/', index, name='index'),
+    path('login/', Login.as_view(), name='login')
 ]
 
 router = SimpleRouter()
--- chat/views.py
+++ chat/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
 from .serializers import UserSerializer
 
 
 def index(request):
-    if request.user.is_authenticated:
-        msg = 'Пользователь авторизован'
-    else:
-        msg = 'Пользователь неизвестен'
-
-    context = {
-        'msg': msg
-    }
-    return render(request, 'chat/index.html', context=context)
+    return render(request, 'chat/index.html', context={})
 
 
 class UserViewSet(ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class Login(LoginView):
+    template_name = 'chat/login.html'
