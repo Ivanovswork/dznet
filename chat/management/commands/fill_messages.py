bf3603b2197eb4f97d7c3e82d55d8e126e54214b
SergeyLebidko <it.sergeyler@mail.ru>
2020-07-17 13:45:50 +0300

    Изменен шаблон главной страницы. Добавлена команда создания сообщений

---
+++ chat/management/commands/fill_messages.py
@@ -0,0 +1,25 @@
+import random
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from chat.models import Message
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        """Команда для тестирования: она удаляет все сообщения из БД и заполняет её тестовыми сообщениями"""
+
+        # Удаляем все старые сообщения из базы
+        Message.objects.all().delete()
+
+        users = list(User.objects.all())
+        for _ in range(len(users) * 50):
+            sender = random.choice(users)
+            receiver = random.choice(users)
+            text = f'Пример текста для сообщения. ' \
+                f'Текст должен быть достаточно длинным и включать случайный элемент: {random.randint(0, 1000000)}'
+            Message.objects.create(
+                text=text,
+                sender=sender,
+                receiver=receiver
+            )
